cf6909924aaaf891745f39a4cce8a2d690edb34e
Gabospa <gabospa@gmail.com>
2020-11-21 17:35:45 -0500

    Configure handlebars

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 
 // Initializations
@@ -13,12 +14,21 @@ app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', exphbs({
     defaultLayout:'main',
-    layoutsDir: path.join(app.get('views'), )
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
 }))
+app.set('view engine', '.hbs')
 
 
 // Middlewares
-
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true,
+}))
 
 // Global Variables
 
