0a7c9ad91ab95260de3e0101aca1760ade3241cc
Gabospa <gabospa@gmail.com>
2020-11-22 07:46:41 -0500

    Fixed signup and login services

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ const path = require('path')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport')
+const { initialize } = require('passport')
+const { allowedNodeEnvironmentFlags } = require('process')
 
 
 // Initializations
 const app = express()
 require('./database')
+require('./config/passport')
 
 
 // Settings
@@ -30,9 +35,22 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
 }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 // Global Variables
-
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    res.locals.error = req.flash('error')
+    let user = null
+    if(req.user){
+        user = JSON.parse(JSON.stringify(req.user))
+    }
+    res.locals.user = user
+    next()
+})
 
 // Routes
 app.use(require('./routes/index'))
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,49 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/User')
+
+const passport = require('passport')
+
 router.get('/users/signup', (req, res) => {
     res.render('users/signup')
+})
+
+router.post('/users/signup', async (req, res) => {
+    const { username, email, password, confirm_password } = req.body
+    const errors = []
+    if (username.length <= 0 || email.length <= 0 ) {
+        errors.push({text: 'Please fill all the fields'})
+    }
+    if (password != confirm_password) {
+        errors.push({text: "Passwords don't match"})
+    }
+    if (password.length < 5) {
+        errors.push({text: 'Password must have 5 or more characters'})
+    }
+    const emailUser = await User.findOne({email: email})
+    if (emailUser){
+        errors.push({text: 'Email is already taken. Please use new one'})
+    }
+    if (errors.length > 0) {
+        res.render('users/signup', {errors, username, email, password, confirm_password})
+    } else {
+        const newUser = new User({ username, email, password})
+        newUser.password = await newUser.encryptPassword(password)
+        await newUser.save()
+        req.flash('success_msg', 'Register Succesfull')
+        res.redirect('/users/signin')
+    }
 })
 
 router.get('/users/signin', (req, res) => {
     res.render('users/signin')
 })
+
+router.post('/users/signin', passport.authenticate('local', {
+    successRedirect: '/surveys',
+    failureRedirect: '/users/signin',
+    failureFlash: true
+}))
 
 module.exports = router
